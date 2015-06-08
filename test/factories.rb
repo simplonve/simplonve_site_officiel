1ffa6a3a7a56612e1964bdc8a775e06b9745fdee
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-06-09 00:08:29 +0200

    tests les validations du modèle article

--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :article do
+    day  01
+    month 'janvier'
+    year 2015
+    title 'Ceci est un titre'
+    subtitle 'Et cela un sous-titre'
+    content 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
   end
 
   factory :comment do
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "article invalid without day" do
+    assert FactoryGirl.build(:article, day: nil).invalid?
+  end
+  
+  test "article invalid without year" do
+    assert FactoryGirl.build(:article, year: nil).invalid?
+  end
+  
+  test "article invalid without month" do
+    assert FactoryGirl.build(:article, month: nil).invalid?
+  end
+  
+  test "article invalid without content" do
+    assert FactoryGirl.build(:article, content: nil).invalid?
+  end
+
+  test "article invalid if shorter than 12 chars" do
+    assert FactoryGirl.build(:article, title: "foo").invalid?
+  end
 end
