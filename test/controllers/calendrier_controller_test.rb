5840b5348d15557c1434f5e9c2bee1accc57a3eb
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-06-11 09:19:39 +0200

    ajout de tests d'intégration FIXME avec 5 erreurs à corriger

--- test/controllers/calendrier_controller_test.rb
+++ test/controllers/calendrier_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CalendrierControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get reservation" do
+    get :reservation
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
 end
