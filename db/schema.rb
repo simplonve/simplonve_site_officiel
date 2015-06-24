03ceffbd023e281d7a0ef70b76a08dff8b5f9def
Xavier07 <albator98@hotmail.com>
2015-06-24 14:17:52 +0200

    intégration de l'affichage des citations dans le site officiel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605123902) do
+ActiveRecord::Schema.define(version: 20150618130545) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "day"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20150605123902) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "citations", force: :cascade do |t|
+    t.text     "citation"
+    t.string   "auteur"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
