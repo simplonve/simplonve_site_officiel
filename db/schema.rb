dfa5fc0bdaef1d332a34ba82eb310d1184deb151
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-06-30 13:59:14 +0200

    ajout dans seed du destroy_all & db user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630092136) do
+ActiveRecord::Schema.define(version: 20150630100145) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "day"
@@ -71,8 +71,11 @@ ActiveRecord::Schema.define(version: 20150630092136) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
