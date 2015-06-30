e479a51d7ec689129f957f06ed664a322c94e5a7
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-06-30 16:45:27 +0200

    travail session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to root_path
     else
-      # Create an error message.
-      render 'new'
+        flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      	render 'new'
     end
   end
 
   def destroy
   end
-  
+
 end
