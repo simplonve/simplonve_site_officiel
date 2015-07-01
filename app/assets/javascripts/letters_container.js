8e4cdb332f57b71c4b31ff0c8a6a28d2052a58ea
thalion26 <thalion26@gmail.com>
2015-07-01 16:10:31 +0200

    modif js map et citations

---
+++ app/assets/javascripts/letters_container.js
@@ -0,0 +1,23 @@
+var lettersContainer = document.getElementById('mainText'),
+	letters, 
+	delay = 180,
+	delayJump = 10,
+	totalLetters;
+
+letterer( lettersContainer );
+
+letters = lettersContainer.getElementsByTagName('letter');
+totalLetters = letters.length;
+
+for( var i=0; i < totalLetters; i++ ){
+	doTimer(letters[i], delay);
+	delay += delayJump;
+	if( letters[i].innerHTML == ' ' )
+		delay += delayJump * 18;
+}
+
+function doTimer(letter, delay){
+	setTimeout(function(){ 
+		letter.removeAttribute('class');
+	}, delay);
+} 
