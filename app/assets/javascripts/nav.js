b5fc8d622e0c0834a7bfa4823664635e91cdd213
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-07-07 11:55:30 +0200

    changement marge et largeur de la nav selon media queries

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
   $("#nav-toggle").on("click", function() {
       $(this).toggleClass("active");
+      var mq = window.matchMedia('all and (max-width: 760px)');
       if($(this).hasClass("active")){
          $(".layout_nav").css("margin-left", "0");
       }else{
-         $(".layout_nav").css("margin-left", "-160px");
+      	if (mq.matches){
+        	$(".layout_nav").css("margin-left", "-90%");
+      	}else{
+        	$(".layout_nav").css("margin-left", "-160px");
+      	}
       }        
   });
 });
