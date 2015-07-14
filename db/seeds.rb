455032f70bd206397f903a753483e2f9cf8f9238
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-07-14 16:03:36 +0200

    add intervenants data into seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,30 @@
 require 'csv'
+
+#Détruire puis reconstruire les données la table Citation
+
 Citation.destroy_all
-csv_text = File.read('./db/citations.csv')
-csv = CSV.parse(csv_text, :headers => true)
-csv.each do |row|
+
+citations_text = File.read('./db/citations.csv')
+citations_ary = CSV.parse(citations, :headers => true)
+
+citations_ary.each do |row|
   citation = Citation.new
   citation.citation = row.to_hash["citation"]
   citation.auteur = row.to_hash["auteur"] 
   citation.save
+end
+
+
+#Construire les données de la table Intervenant
+
+intervenant_text = File.read('./db/intervenants.csv')
+intervenants_ary = CSV.parse(intervenants, :headers => true)
+
+intervenants_ary.each do |row|
+  intervenant = Calendrier.new
+  intervenant.nom =         row.to_hash["nom"]
+  intervenant.prenom =      row.to_hash["prenom"] 
+  intervenant.description = row.to_hash["description"] 
+  intervenant.date =        row.to_hash["date"] 
+  intervenant.save
 end
