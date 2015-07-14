b65497cc19f2db53ef6557c85f0968aed457e055
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-07-14 16:18:21 +0200

    améliration affichage intervenants

--- app/models/calendrier.rb
+++ app/models/calendrier.rb
@@ -1,7 +1,7 @@
 class Calendrier < ActiveRecord::Base
-	validates :nom, presence: true
+	validates :nom, presence: true, uniqueness: true
  	validates :prenom, presence: true
  	validates :email, presence: true
  	validates :telephone, presence: true
-  	validates :date, presence: true
+	validates :date, presence: true
 end
