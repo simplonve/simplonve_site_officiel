366d1401b92e54541a1deeb3e05d5a26ff32184e
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-07-14 16:31:49 +0200

    delete calendrier validation

--- app/models/calendrier.rb
+++ app/models/calendrier.rb
@@ -1,7 +1,5 @@
 class Calendrier < ActiveRecord::Base
 	validates :nom, presence: true, uniqueness: true
  	validates :prenom, presence: true
- 	validates :email, presence: true
- 	validates :telephone, presence: true
 	validates :date, presence: true
 end
