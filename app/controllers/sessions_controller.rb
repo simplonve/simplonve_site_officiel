52deab53e184f49a8e1fd719baf74f126657f922
svey <stephane.vey@laposte.net>
2015-07-17 11:59:58 +0200

    affichage page réservation que si authentifier

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -6,14 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = "ok"
       redirect_to root_path
     else
         flash[:danger] = 'Invalid email/password combination' # Not quite right!
-      	render 'new'
+        render 'new'
     end
   end
 
   def destroy
+      log_out if logged_in?
+      log_out
+      redirect_to root_url
   end
 
 end
