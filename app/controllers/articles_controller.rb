e9d95176557c7ee902b7714b8e378d0dde684be4
Paul van Bommel <boisdecombier@gmail.com>
2015-07-21 16:08:15 +0200

    redirect to index after edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 
 		if @article.update(article_params)
-  		redirect_to @article
+  		redirect_to articles_path
 		else
   		render 'edit'
 		end
