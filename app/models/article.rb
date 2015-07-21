b6bb95ca3b4c6223a7c29f9afb8d2a9910052162
Paul van Bommel <boisdecombier@gmail.com>
2015-07-21 16:09:19 +0200

    no comments table anymore

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
 	validates :day, :month, :year,:content, presence: true
 	validates :title, presence: true
 	validates :subtitle, presence: true
--- test/controllers/comments_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class CommentsControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-end
