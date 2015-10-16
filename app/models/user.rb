68ec628ba686ba522c3fd10e6b1e9b87f0ddfd2c
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-10-16 10:19:53 +0200

    edit form ok with logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ActiveRecord::Base
 				format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }
 	has_secure_password
-	validates :password, presence: true
 
   scope :simploniens, -> {
     where(:online => true)
