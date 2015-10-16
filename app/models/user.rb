6c7170331ac275ac2ff208a222110483deb2d894
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-10-16 14:07:52 +0200

    user model, email presence false

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
 	validates :name,  presence: true
   validates_uniqueness_of :name
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true,
-				format: { with: VALID_EMAIL_REGEX },
-				uniqueness: { case_sensitive: false }
 	has_secure_password
 
   scope :simploniens, -> {
