abd67a725f7eb024f624d62b6de21ec756bff06c
rodinux <rodolphe.robles@sfr.fr>
2016-03-03 18:12:57 +0100

    change a bad link in the redirection when updating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update user_params
-      redirect_to home_promo_path
+      redirect_to promo_path
     else
       render 'edit', notice: "#{user_params} n'est pas disponible"
     end
